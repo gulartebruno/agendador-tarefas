88303ca08f08af002130908a5b38751a5e4840c8
gulartebruno <brunongularte@gmail.com>
2025-01-12 21:23:29 -0300

    correções na usuario client

--- src/main/java/com/bruno/agendador_tarefas/infrastructure/client/UsuarioClient.java
+++ src/main/java/com/bruno/agendador_tarefas/infrastructure/client/UsuarioClient.java
@@ -3,7 +3,6 @@ package com.bruno.agendador_tarefas.infrastructure.client;
 
 import com.bruno.agendador_tarefas.business.dto.UsuarioDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
