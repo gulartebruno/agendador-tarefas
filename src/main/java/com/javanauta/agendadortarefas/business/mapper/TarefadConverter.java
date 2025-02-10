5c4c1324fbd5b69ddb0f6852a61b453c59062794
gulartebruno <brunongularte@gmail.com>
2025-02-10 14:15:24 -0300

    adicionado método post

---
+++ src/main/java/com/javanauta/agendadortarefas/business/mapper/TarefadConverter.java
@@ -0,0 +1,16 @@
+package com.javanauta.agendadortarefas.business.mapper;
+
+
+import com.javanauta.agendadortarefas.business.dto.TarefasDTO;
+import com.javanauta.agendadortarefas.infrastructure.entity.TarefasEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface TarefadConverter {
+
+    TarefasEntity paraTarefaEntity(TarefasDTO dto);
+
+    TarefasDTO paraTarefaDTO(TarefasEntity entity);
+
+
+}
